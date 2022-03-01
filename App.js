94603d32360d975d22c2e745a10c895385cbe160
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-03-01 13:27:34 +0530

    Class Component and its state

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, Text, View, SafeAreaView} from 'react-native';
 import Login from './components/Login';
 
-const App = () => {
-  return (
-    <View>
-      {/* <Text>The Demo Project</Text> */}
-      <Login data={'Some data'} />
-    </View>
-  );
-};
+// const App = () => {
+//   return (
+//     <View>
+//       {/* <Text>The Demo Project</Text> */}
+//       <Login data={'Some data'} />
+//     </View>
+//   );
+// };
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: 'Some class state',
+    };
+  }
+
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>{this.state.data}</Text>
+        <Button
+          title="Change State"
+          onPress={() => {
+            this.setState({data: 'State Changed'});
+          }}
+        />
+      </SafeAreaView>
+    );
+  }
+}
 
 export default App;
