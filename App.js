23122e91d37d621b008033a886a466b8090ad0e2
Dhanajit Kapali <dhanajit.kapali@unthinkable.co>
2022-03-01 13:06:56 +0530

    Login Component

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Login from './components/Login';
+
+const App = () => {
+  return (
+    <View>
+      {/* <Text>The Demo Project</Text> */}
+      <Login />
+    </View>
+  );
+};
+
+export default App;
