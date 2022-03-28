5013aa6302866b87c9cdc9659fc48b4446066ce1
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-03-28 14:00:44 +0530

    stack navigation

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Pressable} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App = () => {
+const Stack = createStackNavigator();
+
+function ScreenA({navigation}) {
+  const onPressHandler = () => {
+    navigation.navigate('Screen_B');
+  };
   return (
-    <View style={styles.body}>
-      <Text>Programming with Mash</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize: 40, fontWeight: 'bold'}}>Screen A</Text>
+      <Pressable onPress={onPressHandler}>
+        <Text>Go to screen B</Text>
+      </Pressable>
     </View>
   );
-};
+}
+
+function ScreenB({navigation}) {
+  const onPressHandler = () => {
+    navigation.goBack();
+  };
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize: 40, fontWeight: 'bold'}}>Screen A</Text>
+      <Pressable onPress={onPressHandler}>
+        <Text>Go Back to screen A</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen_A" component={ScreenA} />
+        <Stack.Screen name="Screen_B" component={ScreenB} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   body: {
