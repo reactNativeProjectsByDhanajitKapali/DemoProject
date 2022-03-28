7b2ab3c3a196bff76732e2154cdeed321b293f7c
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-03-28 12:36:22 +0530

    button

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, Button} from 'react-native';
 
 const App = () => {
+  const [val, setVal] = useState(0);
+  const [buttonPressedVal, setButtonPressedVal] = useState(0);
+
+  const updateData = () => {
+    setVal(val + 5);
+    setButtonPressedVal(buttonPressedVal + 1);
+  };
+
   return (
     <View style={styles.body}>
-      <Text>Programming with Mash</Text>
+      <Text>Total Value {val}</Text>
+      <Button title="Add 5" onPress={updateData}></Button>
+      <Text>Button Pressed {buttonPressedVal}</Text>
     </View>
   );
 };
